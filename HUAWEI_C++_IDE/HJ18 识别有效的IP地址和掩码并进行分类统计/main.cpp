8e595fcdafb05f54f346d9ecc31cb55ac57d6fce
unknown <perrymason568@163.com>
2022-07-12 11:36:33 +0800

    updata 2022/07/12

--- HUAWEI_C++_IDE/HJ18 识别有效的IP地址和掩码并进行分类统计/main.cpp
+++ HUAWEI_C++_IDE/HJ18 识别有效的IP地址和掩码并进行分类统计/main.cpp
@@ -3,6 +3,17 @@
 #include<string>
 #include<sstream>
 using namespace std;
+bool isIgnore(string ip) {
+	istringstream iss(ip);
+	string sub;
+	//getline(iss, sub, '.');
+	int first = stoi(ip.substr(0, ip.find_first_of('.')));
+	if (first==127||first==0) {
+		return true;
+	}
+	return false;
+
+}
 
 bool isLegalIp(string ip) {
 	int count = 0;
@@ -13,18 +24,40 @@ bool isLegalIp(string ip) {
 			return false;
 		}
 	}
-	return j == 4;
+	return count == 4;
 }
 bool isLegalMask(string ip) {
 	istringstream iss(ip);
 	string sub;
-	unsigned b = 0;
+	unsigned int b = 0;
 	//vector<int> v;
+	
 	while (getline(iss, sub, '.'))  b = (b << 8) + stoi(sub);
-	if (!b) return false;
+	if (b == 0 || b == 0xFFFFFFFF || (((b ^ 0xFFFFFFFF) + 1) | b) != b) {
+		return false;
+	}
+	return true;
+	
+	/*
+	while(getline(iss,sub,'.')) b = (b << 8) + stoi(sub);
+    if(!b) return false;
+    b = ~b + 1;
+    if(b == 1) return false;
+    if((b & (b-1)) == 0) return true;
+	return false;
+	*/
+
+	
 }
 bool isPrivateIp(string ip) {
-
+	istringstream iss(ip);
+	string sub;
+	vector<int> sub_vec;
+	while (getline(iss, sub, '.')) sub_vec.push_back(stoi(sub));
+	if (sub_vec[0]==10) return true;
+	if (sub_vec[0] == 172 && (sub_vec[1] >= 16 && sub_vec[1] <= 31)) return true;
+	if (sub_vec[0] == 192 && sub_vec[1]==168 ) return true;
+	return false;
 }
 
 
@@ -40,7 +73,9 @@ int main() {
 			v.push_back(add);
 			//cout << add << endl;
 		}
-		if (!isLegalIp(v[0]) || !isLegalMask(v[1])) {
+		//int first_0_127 = stoi(v[0].subStr(0, v[0].find_first_of('.')));
+		if (isLegalIp(v[0]) && isIgnore(v[0])) continue;
+		if (!isLegalIp(v[1]) || !isLegalMask(v[1])) {
 			Error += 1;
 		}
 		else {
@@ -48,12 +83,22 @@ int main() {
 				Error += 1;
 			}
 			else {
-				isPrivateIp(v[0]);
+				int first = stoi(v[0].substr(0,v[0].find_first_of('.')));
+				//if (first == 127 || first == 0) continue;
+				if (isPrivateIp(v[0])) P++;
+				if (first > 0 && first < 127) A++;
+				else if (first > 127 && first < 192) B++;
+				else if (first > 191 && first < 224) C++;
+				else if (first > 223 && first < 240) D++;
+				else if(first > 239 && first < 256) E++;
 				
 			}
 		}
 	}
 	//输入完之后ctrl+Z结束cin输入循环
-	cout << "quit" << endl;
+	//cout << "quit" << endl;
+	cout << A << " " << B<<" "<<C<<" "<<D<<" "<<E<<" "<<Error<<" "<<P<<" " << endl;
 	return 0;
 }
+
+
